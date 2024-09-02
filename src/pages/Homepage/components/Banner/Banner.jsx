164049f1fc72b7556370dfe40c1f967232866bd8
salted26 <151119318+salted26@users.noreply.github.com>
2024-09-02 17:26:18 +0900

    Update Banner.jsx

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -7,30 +7,33 @@ const Banner = () => {
 
     const {data, isLoading, isError, error} = usePopularMoviesQuery();
 
-    console.log("data", data)
-
     const image = `https://image.tmdb.org/t/p/w533_and_h300_bestv2${data?.results[0].poster_path}`;
     const url = "url(" + image + ")";
 
-    if (isLoading) {
-        return (
-            <div>
-                <h5>Loading...</h5>
-            </div>
-        )
+    const overview = () => {
+        let content = data?.results[0].overview.length;
+        if(content > 150) {
+            content = data?.results[0].overview.substring(0, 150) + "...";
+        } else {
+            content = '';
+        }
+        console.log(content)
+        return content;
+    }
+
+    if(isLoading){
+        return (<div> <h5>Loading.... </h5></div> )
     }
     if (isError) {
-        return (
-            <div>
-                <Alert varian="danger">{error.message}</Alert>
-            </div>
-        )
+        return (<div> <Alert varian="danger">{error.message}</Alert> </div>)
     }
     return (
         <div className="banner-container" style={{backgroundImage:url}} >
             <div className="title-container">
                 <h1>{data?.results[0].title}</h1>
-                <p>{data?.results[0].overview}</p>
+                <p>
+                    {overview()}
+                </p>
             </div>
         </div>
     );
